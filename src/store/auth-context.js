956b8b20786d7383bf08753491c4939ed3d7d599
Ed Bradley <ed3radley@gmail.com>
2023-02-02 16:41:30 +0000

    Switch to using AuthContextProvider component to handle all authentication

--- src/store/auth-context.js
+++ src/store/auth-context.js
@@ -1,7 +1,43 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 
 const AuthContext = React.createContext({
     isLoggedIn: false,
+    // added here for better IDE auto-completion
+    onLogout: () => { },
+    onLogin: (email, password) => { },
 });
+
+export const AuthContexProvider = (props) => {
+    const [isLoggedIn, setIsLoggedIn] = useState(false);
+
+    useEffect(() => {
+        const storedLoggedInInfo = localStorage.getItem('isLoggedIn');
+        if (storedLoggedInInfo === '1') {
+          setIsLoggedIn(true);
+        }
+    
+      }, [])
+
+    const logoutHandler = () => {
+        localStorage.removeItem('isLoggedIn');
+        setIsLoggedIn(false);
+    };
+
+    const loginHandler = () => {
+        localStorage.setItem('isLoggedIn', '1');
+        setIsLoggedIn(true);
+    }
+
+    return (
+    <AuthContext.Provider
+        value={{
+            isLoggedIn: isLoggedIn,
+            onLogout: logoutHandler,
+            onLogin: loginHandler
+        }}>
+        {props.children}
+    </AuthContext.Provider>
+    )
+}
 
 export default AuthContext;
